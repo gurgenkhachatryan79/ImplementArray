be19dfe36164f8c734ffbb970c75fa05812da7b4
gurgenkhachatryan <gkhach83@mail.ru>
2022-01-24 15:18:18 +0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 5;
+            Console.WriteLine(a+b);
+            Console.WriteLine("Hello ssssssssssWorld!!-----!!");
+        }
+    }
+}
